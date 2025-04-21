ca7f32119b68bef5f75d3ddfcca773099f1473ed
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2025-04-21 13:43:42 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,20 @@
 import os
+import shutil
 import subprocess
-
 from getconfig import get_config
 
 
 def preprocess(config):
     os.system("chcp 65001")
-
-    os.environ['TRANSFORMERS_CACHE'] = config["model_path"]
-    os.environ['HTTP_PROXY'] = config["proxy"]
-    os.environ['HTTPS_PROXY'] = config["proxy"]
-
     # 遍历文件夹中的所有文件
     for root, dirs, files in os.walk(config["pre_path"]):
         for filename in files:
             if filename.endswith((".mp4", ".mkv", ".avi", ".mov", ".flv")):
                 video_path = os.path.join(root, filename)
 
-                # 定义输出音频文件的路径
                 basename = os.path.splitext(filename)[0]
                 audio_output_path = os.path.join(root, f"{basename}.wav")
 
-                # 使用 ffmpeg 提取音频
                 command = [
                     "ffmpeg", "-i", video_path,  # 输入视频文件
                     "-vn",  # 不处理视频流，只提取音频
@@ -35,25 +28,39 @@ def preprocess(config):
 
                 print(f"已提取音频并保存至：{audio_output_path}")
 
-
-
-
-
+    # 假设 config 是已经定义好的字典，包含路径等配置信息
     for root, dirs, files in os.walk(config["pre_path"]):
         for filename in files:
             if filename.endswith((".wav", ".mp3", ".flac")):
                 input_audio = os.path.join(root, filename)
 
+                audio_basename = os.path.splitext(os.path.basename(input_audio))[0]
 
+                # Step 1: 运行 demucs 分离人声
                 command = [
-                    "demucs", "--two-stems", "vocals",  # 只分离人声，或用 --out=output_dir 直接分离全部轨道
-                    "-o", config["work_path"],
+                    "demucs","-n", "mdx_extra_q", "--two-stems", "vocals",  # 只分离人声
+                    "-o", config["work_path"],  # 输出到 work_path 文件
+                    "--segment", "60",
+
                     input_audio
                 ]
+                subprocess.run(command, check=True)
 
-                subprocess.run(command)
+                # Step 2: 获取 demucs 输出目录路径 (这里 demucs 会创建一个 htdemucs 文件夹)
+                subfolder = os.path.join(config["work_path"], "htdemucs", audio_basename)
+                vocals_path = os.path.join(subfolder, "vocals.wav")
 
-                print(f"提取人声保存至：{input_audio}")
+                # Step 3: 确保将提取的 vocals.wav 文件移动到目标文件夹
+                final_output_path = os.path.join(config["work_path"], f"{audio_basename}_vocals.wav")
+
+                if os.path.exists(vocals_path):
+                    shutil.move(vocals_path, final_output_path)  # 将文件移动到目标文件夹
+                    print(f"已提取人声：{final_output_path}")
+
+                # 删除 demucs 创建的临时文件夹
+                htdemucs_folder = os.path.join(config["work_path"], "htdemucs")
+                shutil.rmtree(htdemucs_folder, ignore_errors=True)  # 删除 htdemucs 文件夹
+                print(f"已删除临时文件夹：{htdemucs_folder}")
 
 
 if __name__ == "__main__":
