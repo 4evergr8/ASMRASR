16bd07cf0f98ff96ea2a5a769d0743a580f226bb
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2025-04-29 14:59:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-import os
+from audio_separator.separator import Separator
 
+# 自定义输出路径
+separator = Separator(output_directory="your/output/folder")
 
+# 加载模型
+separator.load_model(model_filename="UVR-Mgfbnfgbgfd")
 
-ffmpeg_path = get_ffmpeg_exe()
-print("自定义 FFmpeg 路径：", ffmpeg_path)
+# 开始分离音频
+output_files = separator.separate("path/to/audio_directory")
