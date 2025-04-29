27bde007cbb5c4cae36d26e12f2be09ade45883e
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2025-04-29 15:18:21 +0800

    Update test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from audio_separator.separator import Separator
-
-
-separator = Separator(output_dir="your/output/folder",model_file_dir='your/output/folder')
-separator.load_model(model_filename="htdemucs_ft.yaml")
-output_files = separator.separate("path/to/audio_directory")
