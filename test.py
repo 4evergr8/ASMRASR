4dac538acf3d1752d9e516ba7d4f9ae2bf8b7527
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2025-04-29 15:17:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 from audio_separator.separator import Separator
 
-# 自定义输出路径
-separator = Separator(output_dir="your/output/folder")
 
-# 加载模型
-separator.load_model(model_filename="UVR-Mgfbnfgbgfd")
-
-# 开始分离音频
+separator = Separator(output_dir="your/output/folder",model_file_dir='your/output/folder')
+separator.load_model(model_filename="htdemucs_ft.yaml")
 output_files = separator.separate("path/to/audio_directory")
